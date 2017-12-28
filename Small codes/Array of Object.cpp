67796a4193be2dc97f334c8ec40a0f61ea272d1b
Tri hotboy <tridoan1998@gmail.com>
2017-12-28 01:16:23 -0800

    making the array

--- Small codes/Array of Object.cpp
+++ Small codes/Array of Object.cpp
@@ -1,36 +1,35 @@
 //Program to return the sum of all elements in the array.
 #include <iostream>
 using namespace std;
+
+//function to make the array
+void sum();
+
 int main()
 {
-    int size;
-    cout << "Enter the size of the array: ";
-    cin >> size;
-    int *numberlist;
+    sum();
+    return 0;
+}
 
+void sum()
+{
+    int size;           //create the size for the array
+    cout << "Enter the size of the array: ";
+    cin >> size;    // input size
+    int *numberlist;    //create the array with the input size
     numberlist = new int[size];
-        delete[] pt;
 
     int counter = 0;
     while(counter < size)
     {
         int element;
-        cout "Enter the element for spot #" size+1 << " in the array: ";
+        cout  << "Enter the element for spot #" << counter+ 1 << " in the array: ";
         cin >> element;
-        numberlist[counter]
+        numberlist[counter] = element;
         counter++;
     }
-    int sum = 0;
-    int numberlist = new array[];
-    for (int i = 0; i <= )
-    int numberlist[10] = {1, 2, 3};
-    int size = *(&numberlist + 1) - numberlist; // Return the size of the array
-    for (int i = 0; i <=size; i++)
-    {
-            sum += a[i];
-    }
-    cout << sum;
-    return 0;
-
+    for (int i = 0; i < size; i++)
+        cout << "Element in spot #" << i + 1 << " is " << numberlist[i] << endl;
+    delete[] numberlist;
 
 }
