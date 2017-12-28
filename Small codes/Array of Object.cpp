cc4e7291647a8deeb198a3947616e673d000d4aa
Tri hotboy <tridoan1998@gmail.com>
2017-12-28 00:43:41 -0800

    array of object

--- Small codes/Array of Object.cpp
+++ Small codes/Array of Object.cpp
@@ -1,8 +1,36 @@
+//Program to return the sum of all elements in the array.
 #include <iostream>
 using namespace std;
 int main()
 {
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+    int *numberlist;
 
+    numberlist = new int[size];
+        delete[] pt;
+
+    int counter = 0;
+    while(counter < size)
+    {
+        int element;
+        cout "Enter the element for spot #" size+1 << " in the array: ";
+        cin >> element;
+        numberlist[counter]
+        counter++;
+    }
+    int sum = 0;
+    int numberlist = new array[];
+    for (int i = 0; i <= )
+    int numberlist[10] = {1, 2, 3};
+    int size = *(&numberlist + 1) - numberlist; // Return the size of the array
+    for (int i = 0; i <=size; i++)
+    {
+            sum += a[i];
+    }
+    cout << sum;
+    return 0;
 
 
 }
