bdb863604d6192b9d8975baf8c256d079ed4ab10
Tri Doan <tridoan1998@gmail.com>
2018-01-14 02:55:50 -0800

    Add files via upload

--- PracticeForCIS22C/src/List.cpp
+++ PracticeForCIS22C/src/List.cpp
@@ -12,9 +12,34 @@
 using namespace std;
 
 int main() {
-	Array l;
-	l.add(3);
-	l.wrap(3);
-	cout << l.here();
+	List<int> L1;
+	if(L1.IsEmpty() == true)
+	{
+		cout << "The list is empty.";
+	}
+	else
+		cout << "The list has element.";
+	cout << endl;
+	for (int i = 0; i <= 10; i++)
+	{
+		L1.insertStop(i);
+	}
+	L1.print();
+	cout << endl;
+	cout << L1.getStart();
+	cout << endl;
+	if(L1.IsEmpty() == true)
+	{
+		cout << "The list is empty.";
+	}
+	else
+		cout << "The list has element.";
+	L1.removeStart();
+	L1.removeStop();
+	cout << endl;
+	L1.getIterator();
+	cout << endl;
+	L1.print();
 	return 0;
+
 }
--- PracticeForCIS22C/src/List.h
+++ PracticeForCIS22C/src/List.h
@@ -11,6 +11,10 @@
 # include <cstddef> //for NULL
 # include <assert.h>
 # include <iostream>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "List.h"
 
 using namespace std;
 
@@ -40,16 +44,149 @@ public:
 
 	List();
 	~List();
-	listdata insertStart(listdata data);
-	listdata insertStop(listdata data);
-	listdata removeStart();
-	listdata removeStop();
+	listdata getStart() const;
+	void insertStart(listdata data);
+	void insertStop(listdata data);
+	bool IsEmpty() const;
+	void removeStart();
+	void removeStop();
 	listdata getIterator();
 	listdata advangeIterator();
 	int getSize();
 	bool off_end();
+	void print();
+
 };
+template <class listdata>
+listdata List<listdata>::getIterator()
+{
+	assert(!off_end());
+	return iterator->data;
+}
+template <class listdata>
+List<listdata>::List()
+{
+	start = NULL;
+	stop = NULL;
+	iterator = NULL;
+	size = 0;
+}
 
+template <class listdata>
+List<listdata>::~List()
+{
+	Node* a = start;
+	Node* b = NULL;
+	while(a !=NULL)
+	{
+		b = a->linknext;
+		delete a;
+		a = b;
+	}
+}
 
+template <class listdata>
+bool List<listdata>::off_end()
+{
+	if(iterator == NULL)
+		return true;
+	else
+		return false;
+}
 
+template <class listdata>
+bool List<listdata>::IsEmpty() const
+{
+	return size == 0;
+}
+template <class listdata>
+listdata List<listdata>::getStart() const
+{
+	assert(!IsEmpty());
+	return start->data;
+}
+
+template <class listdata>
+void List<listdata>::insertStart(listdata data)
+{
+	Node* N = new Node(data);
+	if(size == 0)
+	{
+		start = stop = N;
+		size++;
+	}
+	else
+	{
+		N->linknext = start;
+		start->linkprevious = N;
+		start = N;
+		size++;
+	}
+}
+
+template <class listdata>
+void List<listdata>::insertStop(listdata data)
+{
+	Node* N = new Node(data);
+	if(size == 0)
+	{
+		stop = start = N;
+	}
+	else
+	{
+		N->linkprevious = stop;
+		stop->linknext = N;
+		stop = N;
+	}
+	size++;
+}
+
+template <class listdata>
+void List<listdata>::removeStart()
+{
+	assert(!IsEmpty());
+	if(size == 1)
+	{
+		delete start;
+		start = stop = NULL;
+	}
+	else
+	{
+		Node* temp = start;
+		start = start->linknext;
+		start->linkprevious = NULL;
+		delete temp;
+	}
+	size--;
+}
+
+template <class listdata>
+void List<listdata>::removeStop()
+{
+	assert(!IsEmpty());
+	if(size == 1)
+	{
+		delete stop;
+		start = stop = NULL;
+	}
+	else
+	{
+		Node* temp = stop;
+		stop = stop->linkprevious;
+		stop->linknext = NULL;
+		delete temp;
+	}
+	size--;
+}
+
+template <class listdata>
+void List<listdata>::print()
+{
+	Node* temp = start;
+	while(temp->linknext != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->linknext;
+	}
+}
 #endif /* LIST_H_ */
